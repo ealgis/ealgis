f97c27fc1615946634ea72e53de80aae02b88f2c
Grahame Bowland <grahame@angrygoats.net>
2015-05-03 00:05:31 +0800

    split frontend and backend out

--- backend/ealgis/mapserver.py
+++ backend/ealgis/mapserver.py
@@ -125,7 +125,7 @@ mapscript.msIO_installStdoutToBuffer()
 
 class Map(object):
     def __init__(self, rev, defn):
-        self.instance = mapscript.mapObj(os.path.expanduser('/app/backend/template.map'))
+        self.instance = mapscript.mapObj(os.path.expanduser('/app/backend/mapserver/template.map'))
         self.instance.imagetype = 'png'
         self.instance.setProjection('init=epsg:%s' % (EAlGIS().get_setting('map_srid')))
         self.rev = rev
