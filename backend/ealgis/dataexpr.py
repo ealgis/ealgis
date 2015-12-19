c7bef88b7e876043a74cc3679546d9f47f97b75e
Grahame Bowland <grahame@angrygoats.net>
2015-12-19 13:23:09 +0800

    switch to modern sqlalchemy query compilation

--- backend/ealgis/dataexpr.py
+++ backend/ealgis/dataexpr.py
@@ -12,34 +12,11 @@ import ealgis
 eal = ealgis.EAlGIS()
 
 
-# taken from: http://stackoverflow.com/questions/5631078/sqlalchemy-print-the-actual-query
-def printquery(statement, bind=None):
-    """
-    print a query, with values filled in
-    for debugging purposes *only*
-    for security, you should always separate queries from their values
-    please also note that this function is quite slow
-    """
-    import sqlalchemy.orm
-    if isinstance(statement, sqlalchemy.orm.Query):
-        if bind is None:
-            bind = statement.session.get_bind(
-                statement._mapper_zero_or_none())
-        statement = statement.statement
-    elif bind is None:
-        bind = statement.bind
-
-    dialect = bind.dialect
-    compiler = statement._compiler(dialect)
-
-    class LiteralCompiler(compiler.__class__):
-        def visit_bindparam(self, bindparam, within_columns_clause=False, literal_binds=False, **kwargs):
-            return super(LiteralCompiler, self).render_literal_bindparam(
-                bindparam, within_columns_clause=within_columns_clause,
-                literal_binds=literal_binds, **kwargs)
-
-    compiler = LiteralCompiler(dialect, statement)
-    return compiler.process(statement)
+def printquery(query):
+    from sqlalchemy.dialects import postgresql
+    return query.statement.compile(
+        dialect=postgresql.dialect(),
+        compile_kwargs={"literal_binds": True})
 
 
 def operatorOperands(tokenlist):
