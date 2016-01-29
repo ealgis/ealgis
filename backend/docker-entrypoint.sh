c3e448bfb3c7113e35407cd3c772979add173d06
Grahame Bowland <grahame@angrygoats.net>
2016-01-29 23:15:33 +0800

    prod-ish setup seems to work

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-wait_for_postgres()
-{
-    while ! /app/develop.sh psql -l >/dev/null 2>&1; do
-        echo "(sleeping 5)"
-        sleep 5
-    done
-    echo "postgresql is up, continuing"
-}
-
 echo "** ealgis: spinning up"
 
 keyfile=/data/secret_key
@@ -17,21 +8,15 @@ if [ ! -f "$keyfile" ]; then
     python -c "import random,string; print ''.join(random.choice(string.letters + string.digits) for x in range(32))" > "$keyfile"
 fi
 
-# install local copy of ealgis
-( cd /app/backend && pip install -e . )
-
 command="$1"
 
 if [ x"$command" = x"uwsgi" ]; then
-    echo "waiting for postgresql to come up..."
-    wait_for_postgres
     echo "syncdb"
     ealgis syncdb
     echo "starting uwsgi"
     rm -f /data/ealgis.sock
-    chown ealgis /data
     cd /app &&
-        su ealgis -c "uwsgi --ini /app/uwsgi/ealgis.uwsgi"
+        uwsgi --ini /app/uwsgi/ealgis.uwsgi
     while true; do
         echo "** uwsgi has quit: sleep 30 **"
         sleep 30
