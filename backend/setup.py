1d20d6bba0ef49cc70955a91e42c738ed1441b32
Grahame Bowland <grahame@angrygoats.net>
2016-11-30 21:55:25 +0800

    bump

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ealgis',
-    version='1.1.0',
+    version='1.2.0',
     description='EAlGIS',
     long_description='Interactive geospatial mapping system',
     author='Grahame Bowland',
