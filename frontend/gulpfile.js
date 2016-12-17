61835c3c37f6a9fc30089f6faad81cc924be55f8
Grahame Bowland <grahame@angrygoats.net>
2016-12-17 13:20:05 +0800

    basic react/TS setup

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,26 +1,43 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
+var gulp = require('gulp');
+var browserify = require('browserify');
+var watchify = require("watchify");
 var source = require('vinyl-source-stream');
-var tsify = require("tsify");
+var tsify = require('tsify');
+var gutil = require("gulp-util");
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var paths = {
     pages: ['src/*.html']
 };
 
-gulp.task("copy-html", function () {
+gulp.task('copy-html', function () {
     return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task("default", ["copy-html"], function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/index.tsx'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+function bundle() {
+    return watchedBrowserify
     .plugin(tsify)
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest("dist"));
-});
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+};
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
