69bb8931876fd1e2b52bf9acf73ca359c34d7514
Keith Moss <keith.moss@landgate.wa.gov.au>
2017-02-03 10:08:55 +0800

    Moving to use the EALGIS fork of ol-react
    Had the unfortunate side-effect of needing to add babel as a dev-dep.
    Oh well.

--- frontend/src/components/MapUI.tsx
+++ frontend/src/components/MapUI.tsx
@@ -6,8 +6,6 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { Map, View, layer, source, Feature, geom } from 'ol-react';
-import TileWMS from '../ol/TileWMS';
-import ImageWMS from '../ol/ImageWMS';
 import * as ol from 'openlayers';
 
 export interface MapUIProps { defn: any }
@@ -18,17 +16,26 @@ export class MapUI extends React.Component<MapUIProps, undefined> {
 
         if(defn.json !== undefined) {
             // FIXME Fix the map definitions
-            const zoom = parseInt(defn.json.map_defaults.zoom);
-            const center = ol.proj.transform([parseFloat(defn.json.map_defaults.lon), parseFloat(defn.json.map_defaults.lat)], 'EPSG:4326', 'EPSG:900913');
-            
+            const zoom = parseInt(defn.json.map_defaults.zoom)
+            const center = ol.proj.transform([parseFloat(defn.json.map_defaults.lon), parseFloat(defn.json.map_defaults.lat)], 'EPSG:4326', 'EPSG:900913')
+            const params = {'LAYERS': 'EALGIS:93493a38', 'TILED': true, 'SRS': 'EPSG:900913'}
+            // const url = "https://localhost:8443/geoserver/gwc/service/wms"
+            const url = "https://localhost:8443/geoserver/EALGIS/wms"
+            // const urls = [
+            //     'https://gs1.localhost:8443/geoserver/gwc/service/wms',
+            //     'https://gs2.localhost:8443/geoserver/gwc/service/wms',
+            //     'https://gs3.localhost:8443/geoserver/gwc/service/wms',
+            //     'https://gs4.localhost:8443/geoserver/gwc/service/wms',
+            // ]
+
             const view = <View zoom={zoom} center={center}/>;
             return <Map view={view}>
                 <layer.Tile>
                     <source.OSM />
                 </layer.Tile>
                 <layer.Image>
-                    <ImageWMS>
-                    </ImageWMS>
+                    <source.ImageWMS url={url} params={params} serverType={"geoserver"}>
+                    </source.ImageWMS>
                 </layer.Image>
             </Map>
         }
