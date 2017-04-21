208830576c5c9555ece73e23ee83f6c8d3af14c2
Keith Moss <keithamoss@gmail.com>
2017-04-21 14:30:27 +0800

    Switching to tab-based layout for the editor panel in preparation for #85

--- frontend/src/components/LayerForm.tsx
+++ frontend/src/components/LayerForm.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import ContentCreate from 'material-ui/svg-icons/content/create';
+import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
+import ImagePalette from 'material-ui/svg-icons/image/palette';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 import { Field, reduxForm } from 'redux-form';
@@ -20,6 +24,12 @@ import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui
 import { red500, grey400 } from 'material-ui/styles/colors';
 
 const required = value => value || value === 0 ? undefined : 'Required'
+
+const styles = {
+  tabBody: {
+      margin: "10px",
+  }
+}
 
 export interface LayerFormProps {
     mapId: number,
@@ -97,162 +107,191 @@ export class LayerForm extends React.Component<LayerFormProps, undefined> {
                 </ToolbarGroup>
             </Toolbar>
 
-            <form style={{margin: 10}} onSubmit={handleSubmit(onSubmit)}>
-                <Field 
-                    name="name" 
-                    component={TextField} 
-                    hintText="Give your layer a name..."
-                    floatingLabelText="Layer name"
-                    floatingLabelFixed={true}
-                    validate={[ required ]} 
-                    fullWidth={true}
-                    autoComplete="off"
-                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Tabs>
+                    {/* START DESCRIBE TAB */}
+                    <Tab
+                        icon={<ContentCreate />}
+                        label="DESCRIBE"
+                    >
+                        <div style={styles.tabBody}>
+                            <Field 
+                                name="name" 
+                                component={TextField} 
+                                hintText="Give your layer a name..."
+                                floatingLabelText="Layer name"
+                                floatingLabelFixed={true}
+                                validate={[ required ]} 
+                                fullWidth={true}
+                                autoComplete="off"
+                            />
 
-                <Field 
-                    name="description" 
-                    component={TextField}
-                    multiLine={true}
-                    rows={2}
-                    hintText="Give your layer a description..."
-                    floatingLabelText="Layer description"
-                    floatingLabelFixed={true}
-                    validate={[ required ]}
-                    fullWidth={true}
-                    autoComplete="off"
-                />
-                
-                <Field
-                    name="geometry"
-                    component={SelectField}
-                    hintText="Choose your geometry..."
-                    floatingLabelText="Geometry"
-                    floatingLabelFixed={true}
-                    validate={[ required ]} 
-                    fullWidth={true}
-                >
-                    {this.geometryTables}
-                </Field>
+                            <Field 
+                                name="description" 
+                                component={TextField}
+                                multiLine={true}
+                                rows={2}
+                                hintText="Give your layer a description..."
+                                floatingLabelText="Layer description"
+                                floatingLabelFixed={true}
+                                validate={[ required ]}
+                                fullWidth={true}
+                                autoComplete="off"
+                            />
+                            
+                            <Field
+                                name="geometry"
+                                component={SelectField}
+                                hintText="Choose your geometry..."
+                                floatingLabelText="Geometry"
+                                floatingLabelFixed={true}
+                                validate={[ required ]} 
+                                fullWidth={true}
+                            >
+                                {this.geometryTables}
+                            </Field>
+                        </div>
+                    </Tab>
+                    {/* END DESCRIBE TAB */}
 
-                <Field 
-                    name="borderSize" 
-                    component={TextField} 
-                    hintText="Border size (pixels)"
-                    floatingLabelText="Border size"
-                    floatingLabelFixed={true}
-                    validate={[ required ]}
-                    fullWidth={true}
-                    type="number"
-                    min="0"
-                    max="20"
-                />
+                    {/* START DATA TAB */}
+                    <Tab
+                        icon={<EditorInsertChart />}
+                        label="DATA"
+                    >
+                        <div style={styles.tabBody}>
+                            <Field 
+                                name="valueExpression" 
+                                component={TextField}
+                                multiLine={true}
+                                rows={2}
+                                hintText="Write an expression..."
+                                floatingLabelText="Value expression"
+                                floatingLabelFixed={true}
+                                fullWidth={true}
+                                autoComplete="off"
+                            />
 
-                <h5 style={{"fontSize": "12px", "color": grey400}}>Border colour</h5>
-                <Field
-                    name="borderColour"
-                    component={ColourPicker}
-                    color={initialValues["borderColour"]}
-                />
+                            <Field 
+                                name="filterExpression" 
+                                component={TextField}
+                                multiLine={true}
+                                rows={2}
+                                hintText="Write a filter expression..."
+                                floatingLabelText="Filter expression"
+                                floatingLabelFixed={true}
+                                fullWidth={true}
+                                autoComplete="off"
+                            />
+                        </div>
+                    </Tab>
+                    {/* END DATA TAB */}
 
-                <div style={{"display": "flex", "width": "100%", "marginBottom": 10, "marginTop": 10}}>
-                    <div style={{"flex": 1, "flexBasis": "auto"}}>
-                        <Field
-                            name="fillColourScheme"
-                            component={SelectField}
-                            hintText="Choose your colour scheme..."
-                            floatingLabelText="Fill colour scheme"
-                            floatingLabelFixed={true}
-                            fullWidth={true}
-                            normalize={normalizeColourSchemes}
-                        >
-                            {this.colourSchemes}
-                        </Field>
-                    </div>
+                    {/* START VISUALISE TAB */}
+                    <Tab
+                        icon={<ImagePalette />}
+                        label="VISUALISE"
+                    >
+                        <div style={styles.tabBody}>
+                            <Field 
+                                name="borderSize" 
+                                component={TextField} 
+                                hintText="Border size (pixels)"
+                                floatingLabelText="Border size"
+                                floatingLabelFixed={true}
+                                validate={[ required ]}
+                                fullWidth={true}
+                                type="number"
+                                min="0"
+                                max="20"
+                            />
 
-                    <div style={{"flex": 1, "flexBasis": "auto"}}>
-                        <h5 style={{"fontSize": "12px", "color": grey400, "marginLeft": "45%"}}>Flip colours</h5>
-                        <Field
-                            name="fillColourScaleFlip"
-                            component={Toggle}
-                            style={{"marginLeft": "45%"}}
-                        />
-                    </div>
-                </div>
+                            <h5 style={{"fontSize": "12px", "color": grey400}}>Border colour</h5>
+                            <Field
+                                name="borderColour"
+                                component={ColourPicker}
+                                color={initialValues["borderColour"]}
+                            />
 
-                <Field 
-                    name="fillColourSchemeLevels" 
-                    component={TextField} 
-                    hintText="Choose the number of colour levels..."
-                    floatingLabelText="Fill colour levels"
-                    floatingLabelFixed={true}
-                    validate={[ required ]}
-                    fullWidth={true}
-                    type="number"
-                    normalize={normalizeColourSchemeLevels}
-                />
+                            <div style={{"display": "flex", "width": "100%", "marginBottom": 10, "marginTop": 10}}>
+                                <div style={{"flex": 1, "flexBasis": "auto"}}>
+                                    <Field
+                                        name="fillColourScheme"
+                                        component={SelectField}
+                                        hintText="Choose your colour scheme..."
+                                        floatingLabelText="Fill colour scheme"
+                                        floatingLabelFixed={true}
+                                        fullWidth={true}
+                                        normalize={normalizeColourSchemes}
+                                    >
+                                        {this.colourSchemes}
+                                    </Field>
+                                </div>
 
-                <Field 
-                    name="fillOpacity" 
-                    component={TextField} 
-                    hintText="Fill opacity (%)"
-                    floatingLabelText="Fill opacity"
-                    floatingLabelFixed={true}
-                    validate={[ required ]}
-                    fullWidth={true}
-                    type="number"
-                    min="0"
-                    max="100"
-                />
+                                <div style={{"flex": 1, "flexBasis": "auto"}}>
+                                    <h5 style={{"fontSize": "12px", "color": grey400, "marginLeft": "45%"}}>Flip colours</h5>
+                                    <Field
+                                        name="fillColourScaleFlip"
+                                        component={Toggle}
+                                        style={{"marginLeft": "45%"}}
+                                    />
+                                </div>
+                            </div>
 
-                <Field 
-                    name="valueExpression" 
-                    component={TextField}
-                    multiLine={true}
-                    rows={2}
-                    hintText="Write an expression..."
-                    floatingLabelText="Value expression"
-                    floatingLabelFixed={true}
-                    fullWidth={true}
-                    autoComplete="off"
-                />
+                            <Field 
+                                name="fillColourSchemeLevels" 
+                                component={TextField} 
+                                hintText="Choose the number of colour levels..."
+                                floatingLabelText="Fill colour levels"
+                                floatingLabelFixed={true}
+                                validate={[ required ]}
+                                fullWidth={true}
+                                type="number"
+                                normalize={normalizeColourSchemeLevels}
+                            />
 
-                <Field 
-                    name="filterExpression" 
-                    component={TextField}
-                    multiLine={true}
-                    rows={2}
-                    hintText="Write a filter expression..."
-                    floatingLabelText="Filter expression"
-                    floatingLabelFixed={true}
-                    fullWidth={true}
-                    autoComplete="off"
-                />
+                            <Field 
+                                name="fillOpacity" 
+                                component={TextField} 
+                                hintText="Fill opacity (%)"
+                                floatingLabelText="Fill opacity"
+                                floatingLabelFixed={true}
+                                validate={[ required ]}
+                                fullWidth={true}
+                                type="number"
+                                min="0"
+                                max="100"
+                            />
 
-                <Field 
-                    name="scaleMin" 
-                    component={TextField} 
-                    hintText="Scale minimum"
-                    floatingLabelText="Scale minimum"
-                    floatingLabelFixed={true}
-                    validate={[ required ]}
-                    fullWidth={true}
-                    type="number"
-                    min="0"
-                    max="100"
-                />
+                            <Field 
+                                name="scaleMin" 
+                                component={TextField} 
+                                hintText="Scale minimum"
+                                floatingLabelText="Scale minimum"
+                                floatingLabelFixed={true}
+                                validate={[ required ]}
+                                fullWidth={true}
+                                type="number"
+                                min="0"
+                                max="100"
+                            />
 
-                <Field 
-                    name="scaleMax" 
-                    component={TextField} 
-                    hintText="Scale maximum"
-                    floatingLabelText="Scale maximum"
-                    floatingLabelFixed={true}
-                    validate={[ required ]}
-                    fullWidth={true}
-                    type="number"
-                    min="0"
-                    max="100"
-                />
+                            <Field 
+                                name="scaleMax" 
+                                component={TextField} 
+                                hintText="Scale maximum"
+                                floatingLabelText="Scale maximum"
+                                floatingLabelFixed={true}
+                                validate={[ required ]}
+                                fullWidth={true}
+                                type="number"
+                                min="0"
+                                max="100"
+                            />
+                        </div>
+                    </Tab>
+                    {/* END VISUALISE TAB */}
+                </Tabs>
 
                 <button type="submit" style={{"display": "none"}} />
             </form>
