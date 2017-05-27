e5ac455432743c7609c62f0b090408f1d4d32d2c
Keith Moss <keithamoss@gmail.com>
2017-05-27 21:40:06 +0800

    Just in case we need this...

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,6 +4,17 @@ command="$1"
 if [ x"$command" = x"webpack" ]; then
     export TERM=xterm
     npm install .
+
+    # Build material-ui-autocomplete-google-places again if we only have src
+    # Temporary workaround until we can merge our changes upstream
+    # if [ ! -d "/frontend/node_modules/material-ui-autocomplete-google-places/lib" ]; then
+    #     cd /frontend/node_modules/material-ui-autocomplete-google-places
+    #     npm install --only=dev .
+    #     npm run build
+    #     cd /frontend
+    # fi
+    # End material-ui-autocomplete-google-places build
+
     webpack --progress --colors --watch
     echo "webpack quit, status $?, sleeping..."
     while /bin/true; do
