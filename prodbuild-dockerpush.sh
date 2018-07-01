badef17ca8d548f1fa00d1bd3cb9933368ec611d
Grahame Bowland <grahame@oreamnos.com.au>
2018-07-01 15:48:37 +0800

    tag and push images with a version

--- prodbuild-dockerpush.sh
+++ prodbuild-dockerpush.sh
@@ -3,11 +3,20 @@
 # push images to Docker Hub
 # @TODO version images
 
+ver="$1"
+
+if [ x"$ver" = x ]; then
+        echo "set a version!"
+        exit 1
+fi
+
 echo pushing prod nginx container
-# docker tag ealgis/nginx ealgis/nginx:`someversion`
-docker push ealgis/nginx
+docker tag ealgis/nginx:latest ealgis/nginx:"$ver"
+docker push ealgis/nginx:latest
+docker push ealgis/nginx:"$ver"
 
 echo pushing prod uwsgi container
-# docker tag ealgis/uwsgi ealgis/uwsgi:`someversion`
-docker push ealgis/uwsgi
+docker tag ealgis/uwsgi:latest ealgis/uwsgi:"$ver"
+docker push ealgis/uwsgi:latest
+docker push ealgis/uwsgi:"$ver"
 
