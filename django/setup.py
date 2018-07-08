6376e131558da52be789030406b309e170c227db
Keith Moss <keithamoss@gmail.com>
2018-07-08 20:54:16 +0800

    Change to a VERSION file

--- django/setup.py
+++ django/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("version.py") as f:
-    exec(f.read())
+with open("VERSION") as f:
+    version = version_file.read().strip()
 
 setup(
     author="Grahame Bowland and Keith Moss",
@@ -9,8 +9,8 @@ setup(
     description="ealgis",
     license="GPL3",
     keywords="gis",
-    url="https://github.com/grahame/dividebatur",
+    url="https://github.com/ealgis/ealgis",
     name="ealgis",
-    version=__version__,
+    version=version,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
 )
