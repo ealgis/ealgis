a1433cbd84b40988f50601b1412f900c8de69c7b
Keith Moss <keithamoss@gmail.com>
2018-07-11 18:13:08 +0800

    Stop our API wrapper from swallowing valid 404 messages that we rely on passing through

--- frontend/src/shared/api/EALGISApiClient.tsx
+++ frontend/src/shared/api/EALGISApiClient.tsx
@@ -1,9 +1,9 @@
-import * as Cookies from "js-cookie"
-import * as qs from "qs"
-import * as Raven from "raven-js"
-import "whatwg-fetch"
-import { beginFetch, finishFetch } from "../../redux/modules/app"
-import { iterate as iterateSnackbar, sendMessage as sendSnackbarMessage } from "../../redux/modules/snackbars"
+import * as Cookies from "js-cookie";
+import * as qs from "qs";
+import * as Raven from "raven-js";
+import "whatwg-fetch";
+import { beginFetch, finishFetch } from "../../redux/modules/app";
+import { iterate as iterateSnackbar, sendMessage as sendSnackbarMessage } from "../../redux/modules/snackbars";
 
 export class EALGISApiClient {
     // Only handles fatal errors from the API
@@ -24,7 +24,7 @@ export class EALGISApiClient {
         )
     }
 
-    public get(url: string, dispatch: Function, params: object = {}): Promise<void> {
+    public get(url: string, dispatch: Function, params: object = {}, allow_404: boolean = true): Promise<void> {
         dispatch(beginFetch())
 
         if (Object.keys(params).length > 0) {
@@ -38,7 +38,7 @@ export class EALGISApiClient {
         })
             .then((response: any) => {
                 dispatch(finishFetch())
-                if (response.status >= 200 && response.status < 300) {
+                if ((response.status >= 200 && response.status < 300) || (allow_404 === true && response.status === 404)) {
                     return response.json().then((json: any) => ({
                         response: response,
                         json: json,
@@ -52,7 +52,7 @@ export class EALGISApiClient {
             .catch((error: any) => this.handleError(error, url, dispatch))
     }
 
-    public post(url: string, body: object, dispatch: any) {
+    public post(url: string, body: object, dispatch: any, allow_404: boolean = false) {
         dispatch(beginFetch())
 
         return fetch(url, {
@@ -66,7 +66,7 @@ export class EALGISApiClient {
         })
             .then((response: any) => {
                 dispatch(finishFetch())
-                if (response.status >= 200 && response.status < 300) {
+                if ((response.status >= 200 && response.status < 300) || (allow_404 === true && response.status === 404)) {
                     return response.json().then((json: any) => ({
                         response: response,
                         json: json,
@@ -80,7 +80,7 @@ export class EALGISApiClient {
             .catch((error: any) => this.handleError(error, url, dispatch))
     }
 
-    public put(url: string, body: object, dispatch: any, quiet: boolean = false) {
+    public put(url: string, body: object, dispatch: any, quiet: boolean = false, allow_404: boolean = false) {
         if (quiet === false) {
             dispatch(beginFetch())
         }
@@ -99,7 +99,7 @@ export class EALGISApiClient {
                     dispatch(finishFetch())
                 }
 
-                if (response.status >= 200 && response.status < 300) {
+                if ((response.status >= 200 && response.status < 300) || (allow_404 === true && response.status === 404)) {
                     return response.json().then((json: any) => ({
                         response: response,
                         json: json,
@@ -113,7 +113,7 @@ export class EALGISApiClient {
             .catch((error: any) => this.handleError(error, url, dispatch))
     }
 
-    public delete(url: string, dispatch: any) {
+    public delete(url: string, dispatch: any, allow_404: boolean = false) {
         dispatch(beginFetch())
 
         return fetch(url, {
@@ -125,7 +125,7 @@ export class EALGISApiClient {
         })
             .then((response: any) => {
                 dispatch(finishFetch())
-                if (response.status >= 200 && response.status < 300) {
+                if ((response.status >= 200 && response.status < 300) || (allow_404 === true && response.status === 404)) {
                     return response
                 } else {
                     var error: any = new Error(response.statusText || response.status)
