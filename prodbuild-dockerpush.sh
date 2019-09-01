0842e3f14d6f6013e8b4f780a267918e93dec6fd
Keith Moss <keithamoss@gmail.com>
2019-09-01 09:57:12 +0800

    oops - fix build scripts

--- prodbuild-dockerpush.sh
+++ prodbuild-dockerpush.sh
@@ -22,14 +22,14 @@ fi
 
 if [ "$CMD" = "frontend" ] || [ "$CMD" = "all" ]; then
         echo pushing prod nginx container
-        docker tag ealgis/nginx:latest ealgis/nginx:"$ver"
+        docker tag ealgis/nginx:latest ealgis/nginx:"$VERSION"
         docker push ealgis/nginx:latest
-        docker push ealgis/nginx:"$ver"
+        docker push ealgis/nginx:"$VERSION"
 fi
 
 if [ "$CMD" = "django" ] || [ "$CMD" = "all" ]; then
         echo pushing prod uwsgi container
-        docker tag ealgis/uwsgi:latest ealgis/uwsgi:"$ver"
+        docker tag ealgis/uwsgi:latest ealgis/uwsgi:"$VERSION"
         docker push ealgis/uwsgi:latest
-        docker push ealgis/uwsgi:"$ver"
+        docker push ealgis/uwsgi:"$VERSION"
 fi
