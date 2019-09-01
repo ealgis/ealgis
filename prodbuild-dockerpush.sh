f2e778513d550ed5985bae65e09bda2e2ccfe3f5
Keith Moss <keithamoss@gmail.com>
2019-09-01 09:38:14 +0800

    Improve prodbuild scripts

--- prodbuild-dockerpush.sh
+++ prodbuild-dockerpush.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 # push images to Docker Hub
-# @TODO version images
 
-ver="$1"
-
-if [ x"$ver" = x ]; then
-        echo "set a version!"
-        exit 1
+if [ ! -f ./django/VERSION ]; then
+    echo "File not found!"
+    return
 fi
 
-echo pushing prod nginx container
-docker tag ealgis/nginx:latest ealgis/nginx:"$ver"
-docker push ealgis/nginx:latest
-docker push ealgis/nginx:"$ver"
+VERSION=`cat ./django/VERSION`
+CMD="$1"
 
-echo pushing prod uwsgi container
-docker tag ealgis/uwsgi:latest ealgis/uwsgi:"$ver"
-docker push ealgis/uwsgi:latest
-docker push ealgis/uwsgi:"$ver"
+if [ x"$VERSION" = x ]; then
+    echo "set a version!"
+    return
+fi
 
+if [ x"$CMD" = x ]; then
+    echo "provide a command - all, frontend or django!"
+    return
+fi
+
+if [ "$CMD" = "frontend" ] || [ "$CMD" = "all" ]; then
+        echo pushing prod nginx container
+        docker tag ealgis/nginx:latest ealgis/nginx:"$ver"
+        docker push ealgis/nginx:latest
+        docker push ealgis/nginx:"$ver"
+fi
+
+if [ "$CMD" = "django" ] || [ "$CMD" = "all" ]; then
+        echo pushing prod uwsgi container
+        docker tag ealgis/uwsgi:latest ealgis/uwsgi:"$ver"
+        docker push ealgis/uwsgi:latest
+        docker push ealgis/uwsgi:"$ver"
+fi
--- prodbuild.sh
+++ prodbuild.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
-\rm -f nginx-prod/build/*.tgz
-mkdir -p nginx-prod/build/
+CMD="$1"
+PUSH="$2"
 
-# build the frontend assets (this takes quite a while due to minification)
-(cd frontend && yarn install && yarn build && cd build && tar czvf ../../build/frontend.tgz .)
+if [ x"$CMD" = x ]; then
+    echo "provide a command - all, frontend or django!"
+    return
+fi
 
-# build the django assets
-docker-compose -f docker-compose-buildpy.yml build
-docker-compose -f docker-compose-buildpy.yml run web
-docker-compose -f docker-compose-buildpy.yml stop
+if [ "$CMD" = "frontend" ] || [ "$CMD" = "all" ]; then
+  \rm -f nginx-prod/build/*.tgz
+  mkdir -p nginx-prod/build/
 
-# build production nginx image
-cp build/frontend.tgz build/django.tgz nginx-prod/build # this is horrible, fixme
+  # build the frontend assets (this takes quite a while due to minification)
+  (cd frontend && yarn install && yarn build && cd build && tar czvf ../../build/frontend.tgz .)
 
-echo building prod nginx container
-(cd nginx-prod && docker build -t ealgis/nginx:latest .)
+  # build the django assets
+  docker-compose -f docker-compose-buildpy.yml build
+  docker-compose -f docker-compose-buildpy.yml run web
+  docker-compose -f docker-compose-buildpy.yml stop
 
-echo building prod uwsgi container
-(cd django && docker build -t ealgis/uwsgi:latest .)
+  # build production nginx image
+  cp build/frontend.tgz build/django.tgz nginx-prod/build # this is horrible, fixme
 
-# remove build artefact
-rm ealgis/ealfront/templates/index.html
+  echo building prod nginx container
+  (cd nginx-prod && docker build -t ealgis/nginx:latest .)
+fi
+
+if [ "$CMD" = "django" ] || [ "$CMD" = "all" ]; then
+  echo building prod uwsgi container
+  (cd django && docker build -t ealgis/uwsgi:latest .)
+
+  # remove build artefact
+  if [ -f django/ealgis/ealfront/templates/index.html ]; then
+    rm django/ealgis/ealfront/templates/index.html
+  fi
+fi
+
+if [ "$PUSH" = "push" ]; then
+    ./prodbuild-dockerpush.sh "$CMD"
+fi
